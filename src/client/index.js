b05fd701392613fd7bfdee765976d5867c10c90d
geekshikha <79321645+geekshikha@users.noreply.github.com>
2021-10-24 23:09:27 +0200

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,14 @@
+import { getTravelData } from "./js/index";
+import { tripCountdown } from "./js/tripCountdown";
+import { printTrip } from "./js/printTrip";
+
+import "./styles/general.scss";
+import "./styles/header.scss";
+import "./styles/body.scss";
+import "./styles/footer.scss";
+
+export { 
+    getTravelData, 
+    tripCountdown,
+    printTrip
+};
