4074e1ba3a4d7ef3d843d1251a3775c60fa937f0
geekshikha <79321645+geekshikha@users.noreply.github.com>
2021-10-24 23:18:47 +0200

    Add files via upload

---
+++ src/client/js/printTrip.js
@@ -0,0 +1,33 @@
+import { jsPDF } from "jspdf";
+const printWrapper = document.getElementById('print-div');
+const contentWrapper = document.querySelector('.bottom');
+
+const printTrip = () => {
+    const printButton = document.createElement("input");
+    printButton.setAttribute("type", "submit");
+    printButton.setAttribute("id", "print-trip");
+    printButton.setAttribute("value", "Print Trip");
+    
+    //For jest
+    window.addEventListener('load', (event) => {
+        printWrapper.appendChild(printButton);
+    })
+
+    const printTrip = () => {
+        const doc = new jsPDF({
+            unit: "mm",
+            format: [700, 700]
+        });
+        doc.html(contentWrapper, {
+            callback: function (doc) {
+                doc.save("trip-details.pdf");
+            },
+            x: 15,
+            y: 15,
+        });
+    }
+
+    printButton.addEventListener("click", printTrip);
+}
+
+export { printTrip }
